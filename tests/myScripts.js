061181eeecaae5785b6095c323130bd358e5aafa
santiagoM05 <55292454+santiagoM05@users.noreply.github.com>
2020-01-15 12:59:56 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -1,1 +1,25 @@
+document.write("Loop A <br/>");
+var i;
+for(i=0; i<31; i++){
+  document.write("my loop:" + i + "<br/>");
+}
 
+document.write("Loop B <br/>");
+
+var j;
+for(j=200; j>149; j--){
+  document.write("my loop:" + j + "<br/>");
+}
+
+document.write("Loop C <br/>");
+var k;
+for(k=100; k<5001; k+=100){
+  document.write("third loop:" + k +"<br/>")
+}
+
+document.write("Power loop <br/>");
+var p;
+for(p= 0; p<9; p++){
+var ans = Math.pow(2,p);
+  document.write("the power is:" + ans + "<br/>");
+}
