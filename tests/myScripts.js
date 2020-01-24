1fdb86c588427708e6b4fe169efc2e26466a6cc5
santiagoM05 <55292454+santiagoM05@users.noreply.github.com>
2020-01-24 13:15:46 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -42,10 +42,20 @@ function myLoop(){
     
 var start = document.getElementById("start").value;
 var end = document.getElementById("end").value;
-for(var i=start; i<= end; i++)
+var jump = document.getElementById("jump").value;
+  
+console.log(typeof start);
+console.log(typeof end);  
+console.log(typeof jump);
+  
+start =Number(start);
+end= Number(end);
+jump= Number(jump);
+  
+for(var i=start; i<= end; i+=jump)
   {
     document.write("<p>"+ i+ "</p>");
   }
-
-
+  
 }
+
